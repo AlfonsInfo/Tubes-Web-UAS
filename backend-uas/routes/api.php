ae6e6c32f69fab032f72be2fef9b3055d5890b93
Alfonsdev <itsmealfons@gmail.com>
2022-12-07 21:17:50 +0700

    Kerangka Dasar untuk Otentikasi dan Otorisasi

--- backend-uas/routes/api.php
+++ backend-uas/routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('register', 'Api\AuthController@register');
+Route::apiResource('login', 'Api\AuthController@login');
+
+Route::group(['middleware' => 'auth:api'], function(){
+    
+});
