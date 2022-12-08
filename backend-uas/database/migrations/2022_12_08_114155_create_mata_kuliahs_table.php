d43e9976a962c081be0189ed64338e0c82bc62b2
Hanwynnn <tanghanwyn@gmail.com>
2022-12-08 11:46:59 +0700

    Model Matakuliah

---
+++ backend-uas/database/migrations/2022_12_08_114155_create_mata_kuliahs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mata_kuliahs', function (Blueprint $table) {
+            $table->id();
+            $table->string("nama_matkul");
+            $table->string("kode_matkul");
+            $table->integer("sks");
+            $table->string("kelas");
+            $table->string("dosen");
+            $table->integer("sesi");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mata_kuliahs');
+    }
+};
